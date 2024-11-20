3d5274126b9f14cb9b05313e9e6a4ceb0a8cea03
noorafsarpiash <noorpiash1234@gmail.com>
2024-11-21 02:57:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import SideBar from "./SideBar";
+import MovieList from "./cine/MovieList";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <SideBar />
+          <MovieList />
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
